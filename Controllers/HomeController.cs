07a248396e67976113873cbd8449f8b31c3eafd9
Julia Szewczyk <juliaszew@student.agh.edu.pl>
2026-06-17 19:09:30 +0200

    Dodanie panelu administratora + naprawa błędów

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,42 @@
 using System.Diagnostics;
 using App_web_Tatry.Models;
+using App_web_Tatry.Entities; 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; 
+
+namespace App_web_Tatry.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context; 
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var szlaki = await _context.Szlaki.Include(s => s.Zdjecia).ToListAsync();
+            return View(szlaki);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+/*
+using System.Diagnostics;
+using App_web_Tatry.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App_web_Tatry.Controllers;
@@ -34,3 +71,4 @@ public class HomeController : Controller
     }
     
 }
+*/
--- Controllers/SzlakiController.cs
+++ Controllers/SzlakiController.cs
@@ -1,4 +1,197 @@
+using App_web_Tatry.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+namespace App_web_Tatry.Controllers
+{
+    // ZMIANA: Blokujemy ca³y kontroler na poziomie klasy wy³¹cznie dla u¿ytkowników z rol¹ Admin
+    [Authorize(Roles = "Admin")]
+    public class SzlakiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public SzlakiController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: Szlaki (Panel administracyjny)
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Szlaki.Include(s => s.Zdjecia).ToListAsync());
+        }
+
+        // GET: Szlaki/Details/5
+        // ZMIANA: Odblokowujemy tê akcjê dla wszystkich (w tym niezalogowanych goœci)
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var szlak = await _context.Szlaki
+                .Include(s => s.Zdjecia)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (szlak == null) return NotFound();
+
+            return View(szlak);
+        }
+        //GET: SZLAKI/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SZLAKS/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create([Bind("Id,Nazwa,Opis,Dlugosc,CzasPrzejscia,PoziomTrudnosci,KolorSzlakow,Zdjecia")] Szlak szlak, List<IFormFile> plikiZdjec)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(szlak);
+                await _context.SaveChangesAsync();
+
+                if (plikiZdjec != null && plikiZdjec.Count > 0)
+                {
+                    string folderZapisu = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+
+                    if (!Directory.Exists(folderZapisu))
+                    {
+                        Directory.CreateDirectory(folderZapisu);
+                    }
+
+                    foreach (var plik in plikiZdjec)
+                    {
+                        if (plik.Length > 0)
+                        {
+                            string unikalnaNazwaPliku = Guid.NewGuid().ToString() + "_" + Path.GetFileName(plik.FileName);
+                            string pelnaSciezkaPliku = Path.Combine(folderZapisu, unikalnaNazwaPliku);
+
+                            using (var stream = new FileStream(pelnaSciezkaPliku, FileMode.Create))
+                            {
+                                await plik.CopyToAsync(stream);
+                            }
+
+                            var noweZdjecie = new Zdjecie
+                            {
+                                SciezkaPliku = "/images/" + unikalnaNazwaPliku,
+                                SzlakId = szlak.Id
+                            };
+
+                            _context.Zdjecia.Add(noweZdjecie);
+                        }
+                    }
+                    await _context.SaveChangesAsync();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(szlak);
+        }
+
+        // GET: SZLAKS/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var szlak = await _context.Szlaki.FindAsync(id);
+            if (szlak == null)
+            {
+                return NotFound();
+            }
+            return View(szlak);
+        }
+
+        // POST: SZLAKS/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id, [Bind("Id,Nazwa,Opis,Dlugosc,CzasPrzejscia,PoziomTrudnosci,KolorSzlakow,Zdjecia")] Szlak szlak)
+        {
+            if (id != szlak.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(szlak);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SzlakExists(szlak.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(szlak);
+        }
+
+        // GET: SZLAKS/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var szlak = await _context.Szlaki
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (szlak == null)
+            {
+                return NotFound();
+            }
+
+            return View(szlak);
+        }
+
+        // POST: SZLAKS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var szlak = await _context.Szlaki.FindAsync(id);
+            if (szlak != null)
+            {
+                _context.Szlaki.Remove(szlak);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SzlakExists(int? id)
+        {
+            return _context.Szlaki.Any(e => e.Id == id);
+        }
+    }
+}
+
+/*
 using App_web_Tatry.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +199,7 @@ using Microsoft.EntityFrameworkCore;
 
 public class SzlakiController : Controller
 {
+    
     private readonly ApplicationDbContext _context;
     private readonly IWebHostEnvironment _webHostEnvironment;
 
@@ -46,6 +240,7 @@ public class SzlakiController : Controller
     {
         return View();
     }
+    
 
     // POST: SZLAKS/Create
     // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -191,3 +386,4 @@ public class SzlakiController : Controller
         return _context.Szlaki.Any(e => e.Id == id);
     }
 }
+*/
